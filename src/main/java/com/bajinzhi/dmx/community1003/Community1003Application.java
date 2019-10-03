4c4f9265c64c60aa3434b5ce27a8805689fb3ef2
Mingxiang <2513458060@qq.com>
2019-10-03 22:58:56 +0800

    init repo

---
+++ src/main/java/com/bajinzhi/dmx/community1003/Community1003Application.java
@@ -0,0 +1,13 @@
+package com.bajinzhi.dmx.community1003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community1003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community1003Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/bajinzhi/dmx/community1003/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.bajinzhi.dmx.community1003.controller;
+
+/**
+ * @BelongsProject: community1003
+ * @BelongsPackage: com.bajinzhi.dmx.community1003.controller
+ * @Author: Administrator
+ * @CreateTime: 2019-10-03 22:01
+ * @Description: ${Description}
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+//    @RequestMapping("/hello")
+    public String hello(@RequestParam(name = "name",required = false,defaultValue = "张三")String name, Model model){
+        System.out.println("11111111111111111111111");
+        model.addAttribute("name",name);
+        System.out.println("name :" + name);
+        return  "hello1";
+    }
+
+
+}
